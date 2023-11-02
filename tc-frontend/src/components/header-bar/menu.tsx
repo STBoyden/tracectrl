7f7d98be084e775e4e21df3efefc5f2da2a417d5
Samuel Boyden <sam@stboyden.com>
2023-11-02 17:21:56 +0000

    Change default host, and rename to websocketHost

--- tc-frontend/src/components/header-bar/menu.tsx
+++ tc-frontend/src/components/header-bar/menu.tsx
@@ -1,21 +1,21 @@
 import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
 } from "@/components/ui/sheet";
 import { useToast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils";
@@ -24,77 +24,77 @@ import { GearIcon } from "@radix-ui/react-icons";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import {
-  _defaultSettings,
-  settingsFormSchema,
-  useSettings,
+	_defaultSettings,
+	settingsFormSchema,
+	useSettings,
 } from "../settings-provider";
 
 export function HeaderBarMenu() {
-  const { toast } = useToast();
-  const { settings, setSettings } = useSettings();
+	const { toast } = useToast();
+	const { settings, setSettings } = useSettings();
 
-  const settingsForm = useForm<z.infer<typeof settingsFormSchema>>({
-    resolver: zodResolver(settingsFormSchema),
-    defaultValues: _defaultSettings,
-  });
+	const settingsForm = useForm<z.infer<typeof settingsFormSchema>>({
+		resolver: zodResolver(settingsFormSchema),
+		defaultValues: _defaultSettings,
+	});
 
-  async function onFormSubmit(values: z.infer<typeof settingsFormSchema>) {
-    setSettings(values);
+	async function onFormSubmit(values: z.infer<typeof settingsFormSchema>) {
+		setSettings(values);
 
-    toast({
-      title: "Saved changes",
-      description: (
-        <>
-          Host changed to <code>{values.host}</code>
-        </>
-      ),
-    });
-  }
+		toast({
+			title: "Saved changes",
+			description: (
+				<>
+					Host changed to <code>{values.websocketHost}</code>
+				</>
+			),
+		});
+	}
 
-  return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline">
-          <GearIcon className={cn("h-4", "mr-2", "w-4")} /> Settings
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left">
-        <SheetHeader>
-          <SheetTitle>Settings</SheetTitle>
-          <SheetDescription>
-            Settings for TraceCTRL can be found here.
-          </SheetDescription>
-        </SheetHeader>
-        <Form {...settingsForm}>
-          <form
-            onSubmit={settingsForm.handleSubmit(onFormSubmit)}
-            className={cn("space-y-4")}
-          >
-            <FormField
-              control={settingsForm.control}
-              name="host"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Host</FormLabel>
-                  <FormControl>
-                    <Input placeholder={settings.host!} {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    The host from where to receive the logs from. By default, it
-                    is targeted at{" "}
-                    <span className={cn("font-mono")}>
-                      {_defaultSettings.host!}
-                    </span>
-                    .
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            ></FormField>
-            <Button type="submit">Save changes</Button>
-          </form>
-        </Form>
-      </SheetContent>
-    </Sheet>
-  );
+	return (
+		<Sheet>
+			<SheetTrigger asChild>
+				<Button variant="outline">
+					<GearIcon className={cn("h-4", "mr-2", "w-4")} /> Settings
+				</Button>
+			</SheetTrigger>
+			<SheetContent side="left">
+				<SheetHeader>
+					<SheetTitle>Settings</SheetTitle>
+					<SheetDescription>
+						Settings for TraceCTRL can be found here.
+					</SheetDescription>
+				</SheetHeader>
+				<Form {...settingsForm}>
+					<form
+						onSubmit={settingsForm.handleSubmit(onFormSubmit)}
+						className={cn("space-y-4")}
+					>
+						<FormField
+							control={settingsForm.control}
+							name="websocketHost"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Host</FormLabel>
+									<FormControl>
+										<Input placeholder={settings.websocketHost!} {...field} />
+									</FormControl>
+									<FormDescription>
+										The host from where to receive the logs via websockets from.
+										By default, it is targeted at{" "}
+										<span className={cn("font-mono")}>
+											{_defaultSettings.websocketHost!}
+										</span>
+										.
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						></FormField>
+						<Button type="submit">Save changes</Button>
+					</form>
+				</Form>
+			</SheetContent>
+		</Sheet>
+	);
 }
--- tc-frontend/src/components/settings-provider.tsx
+++ tc-frontend/src/components/settings-provider.tsx
@@ -1,78 +1,79 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useState } from "react";
 import * as z from "zod";
 
 const settingsKey = "tracectrl-settings" as const;
 
 type Settings = {
-  host?: string;
+	websocketHost?: string;
 };
 
 export const _defaultSettings: Settings = {
-  host: "localhost:3002",
+	websocketHost: "localhost:3001",
 };
 
 export const settingsFormSchema = z.object({
-  host: z
-    .union([
-      z.string().url("Invalid URL"),
-      z
-        .string()
-        .ip({ version: "v4", message: "IP address has to be in ipv4 format" }),
-    ])
-    .optional()
-    .default(_defaultSettings.host!),
+	websocketHost: z
+		.union([
+			z.string().url("Invalid URL"),
+			z
+				.string()
+				.ip({ version: "v4", message: "IP address has to be in ipv4 format" }),
+		])
+		.optional()
+		.default(_defaultSettings.websocketHost!),
 });
 
 type SettingsProviderProps = {
-  children: React.ReactNode;
-  defaultSettings?: Settings;
+	children: React.ReactNode;
+	defaultSettings?: Settings;
 };
 
 type SettingsProviderState = {
-  settings: Settings;
-  setSettings: (settings: Settings) => void;
+	settings: Settings;
+	setSettings: (settings: Settings) => void;
 };
 
 const initialState: SettingsProviderState = {
-  settings: _defaultSettings,
-  setSettings: () => null,
+	settings: _defaultSettings,
+	setSettings: () => null,
 };
 
 const SettingsProviderContext =
-  createContext<SettingsProviderState>(initialState);
+	createContext<SettingsProviderState>(initialState);
 
 export function SettingsProvider({
-  children,
-  defaultSettings = _defaultSettings,
-  ...props
+	children,
+	defaultSettings = _defaultSettings,
+	...props
 }: SettingsProviderProps) {
-  const [settings, setSettings] = useState<Settings>(
-    () =>
-      (JSON.parse(
-        localStorage.getItem(settingsKey) ?? JSON.stringify("")
-      ) as Settings) || defaultSettings
-  );
+	const [settings, setSettings] = useState<Settings>(
+		() =>
+			(JSON.parse(
+				localStorage.getItem(settingsKey) ?? JSON.stringify(""),
+			) as Settings) || defaultSettings,
+	);
 
-  const value = {
-    settings,
-    setSettings: (settings: Settings) => {
-      localStorage.setItem(settingsKey, JSON.stringify(settings));
-      setSettings(settings);
-    },
-  };
+	const value = {
+		settings,
+		setSettings: (settings: Settings) => {
+			localStorage.setItem(settingsKey, JSON.stringify(settings));
+			setSettings(settings);
+		},
+	};
 
-  return (
-    <SettingsProviderContext.Provider {...props} value={value}>
-      {children}
-    </SettingsProviderContext.Provider>
-  );
+	return (
+		<SettingsProviderContext.Provider {...props} value={value}>
+			{children}
+		</SettingsProviderContext.Provider>
+	);
 }
 
 export function useSettings() {
-  const context = useContext(SettingsProviderContext);
+	const context = useContext(SettingsProviderContext);
 
-  if (context === undefined)
-    throw new Error("useSettings must be used within a SettingsProvider");
+	if (context === undefined)
+		throw new Error("useSettings must be used within a SettingsProvider");
 
-  return context;
+	return context;
 }
